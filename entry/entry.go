a631047c87ba7663ee61c5abe17b272d8e618d93
ps120 <ps120_studio@outlook.com>
2023-08-14 14:27:24 +0800

    sdk version update

--- entry/entry.go
+++ entry/entry.go
@@ -10,8 +10,9 @@ import (
 )
 
 const (
-	callModelFuncApi = "http://192.168.0.68:8082/api/model/func/url"
-	emitEventApi     = "http://192.168.0.68:8082/api/model/func/url"
+	getModelFunUrlApi  = "http://192.168.0.68:8082/api/model/func/url"
+	getModelHookUrlApi = "http://192.168.0.68:8082/api/model/func/hook/url"
+	emitEventApi       = "http://192.168.0.68:8082/api/model/func/url"
 )
 
 func GetModelFuncUrl(modelId string, methodName string) (url string, err error) {
@@ -19,38 +20,17 @@ func GetModelFuncUrl(modelId string, methodName string) (url string, err error) 
 		"model_db_id": modelId,
 		"func_name":   methodName,
 	}
-	bData, err := json.Marshal(callFuncReq)
-	if err != nil {
-		return
+	url, err = getUrl(callFuncReq, getModelFunUrlApi)
+	return
+}
+
+func GetModelHookUrl(modelId string, hookName string) (url string, err error) {
+	var hookUrlReq = map[string]any{
+		"model_db_id": modelId,
+		"hook_name":   hookName,
 	}
 
-	request, err := http.NewRequest(http.MethodPost, callModelFuncApi, bytes.NewReader(bData))
-	if err != nil {
-		return
-	}
-
-	var client = http.Client{}
-
-	httpResp, err := client.Do(request)
-	if err != nil {
-		return
-	}
-
-	buff := make([]byte, httpResp.ContentLength)
-	httpResp.Body.Read(buff)
-
-	var result map[string]any
-	err = json.Unmarshal(buff, &result)
-	if err != nil {
-		return
-	}
-
-	if result["errcode"].(int) != 0 {
-		err = errors.New(result["msg"].(string))
-		return
-	}
-
-	url = "http://" + result["data"].(string)
+	url, err = getUrl(hookUrlReq, getModelHookUrlApi)
 	return
 }
 
@@ -92,5 +72,41 @@ func EmitEvent(httpReq *http.Request, eventName string, params any) (err error) 
 		err = errors.New(result["msg"].(string))
 		return
 	}
+	return
+}
+
+func getUrl(req any, api string) (url string, err error) {
+	bData, err := json.Marshal(req)
+	if err != nil {
+		return
+	}
+
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(bData))
+	if err != nil {
+		return
+	}
+
+	var client = http.Client{}
+
+	httpResp, err := client.Do(request)
+	if err != nil {
+		return
+	}
+
+	buff := make([]byte, httpResp.ContentLength)
+	httpResp.Body.Read(buff)
+
+	var result map[string]any
+	err = json.Unmarshal(buff, &result)
+	if err != nil {
+		return
+	}
+
+	if result["errcode"].(int) != 0 {
+		err = errors.New(result["msg"].(string))
+		return
+	}
+
+	url = "http://" + result["data"].(string)
 	return
 }
